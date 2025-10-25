187c32daee4f84b5d75a6cf1646424670217da75
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-25 12:28:09 -0700

    215 Creating DB Table with Python

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-# ## CREATE TABLE "Seat" (
-# 	"seat_id"	TEXT,
-# 	"taken"	INTEGER,
-# 	"price"	REAL
-# );
+"""
+CREATE TABLE "Seat" (
+	"seat_id"	TEXT,
+	"taken"	INTEGER,
+	"price"	REAL
+);
+"""
+import sqlite3
+
+connection = sqlite3.connect("cinema.db")
+connection.execute("""
+    CREATE TABLE "Seat" (
+	"seat_id"	TEXT,
+	"taken"	INTEGER,
+	"price"	REAL
+    );
+    """)
+connection.commit()
+connection.close()
+
